3c426a7e31b21654de983873f0b43f0fef689a47
Benjamin Kircher <bkircher@0xadd.de>
2021-05-29 13:01:10 +0200

    Add an endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
+use dropshot::endpoint;
 use dropshot::ApiDescription;
 use dropshot::ConfigDropshot;
 use dropshot::ConfigLogging;
 use dropshot::ConfigLoggingLevel;
+use dropshot::HttpError;
+use dropshot::HttpResponseOk;
 use dropshot::HttpServerStarter;
+use dropshot::RequestContext;
+use schemars::JsonSchema;
+use serde::Serialize;
 use std::sync::Arc;
+
+#[derive(Serialize, JsonSchema)]
+struct Guest {
+    /** name of this guest */
+    name: String,
+}
+
+#[endpoint {
+    method = GET,
+    path = "guests/guest1"
+}]
+async fn get_guest(_rqctx: Arc<RequestContext<()>>) -> Result<HttpResponseOk<Guest>, HttpError> {
+    let guest = Guest {
+        name: String::from("some guest"),
+    };
+    Ok(HttpResponseOk(guest))
+}
 
 #[tokio::main]
 async fn main() -> Result<(), String> {
@@ -13,15 +36,16 @@ async fn main() -> Result<(), String> {
     .to_logger("minimal-example")
     .map_err(|e| e.to_string())?;
 
-    let api = ApiDescription::new();
+    let mut api = ApiDescription::new();
+    api.register(get_guest).unwrap();
 
     let server = HttpServerStarter::new(
         &ConfigDropshot {
-            bind_address: "127.0.0.1:0".parse().unwrap(),
+            bind_address: "127.0.0.1:8080".parse().unwrap(),
             request_body_max_bytes: 1024,
         },
         api,
-        Arc::new(()),
+        *Arc::new(()),
         &log,
     )
     .map_err(|error| format!("failed to start server: {}", error))?
